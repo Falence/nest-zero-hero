4a8a3277ed4d66d7a60f13000a2c96e6ca73700e
Falence Lemungoh <falencelemungoh@gmail.com>
2021-10-29 21:14:29 +0100

    feature getting a task by ID

--- dist/tasks/tasks.controller.d.ts
+++ dist/tasks/tasks.controller.d.ts
@@ -5,5 +5,6 @@ export declare class TasksController {
     private tasksService;
     constructor(tasksService: TasksService);
     getAllTasks(): Task[];
+    getTaskById(id: string): Task;
     creatTask(createTaskDto: CreateTaskDto): Task;
 }
