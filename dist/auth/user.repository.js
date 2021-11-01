fd8db820d3a53b2358d5697af14ebd977f7cf70b
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-01 20:02:22 +0100

    feature: user sign up

--- dist/auth/user.repository.js
+++ dist/auth/user.repository.js
@@ -10,6 +10,13 @@ exports.UserRepository = void 0;
 const typeorm_1 = require("typeorm");
 const user_entity_1 = require("./user.entity");
 let UserRepository = class UserRepository extends typeorm_1.Repository {
+    async signUp(authCredentialsDto) {
+        const { username, password } = authCredentialsDto;
+        const user = new user_entity_1.User();
+        user.username = username;
+        user.password = password;
+        await user.save();
+    }
 };
 UserRepository = __decorate([
     (0, typeorm_1.EntityRepository)(user_entity_1.User)
