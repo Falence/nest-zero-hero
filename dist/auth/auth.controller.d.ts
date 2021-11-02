bdbb7b4b7cb37f04817ef8debad105ac6593f3ac
Falence Lemungoh <falencelemungoh@gmail.com>
2021-11-02 15:43:46 +0100

    custom @GetUser() decorator

--- dist/auth/auth.controller.d.ts
+++ dist/auth/auth.controller.d.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
 export declare class AuthController {
     private authService;
     constructor(authService: AuthService);
@@ -7,4 +8,5 @@ export declare class AuthController {
     signIn(authCredentialsDto: AuthCredentialsDto): Promise<{
         accessToken: string;
     }>;
+    test(user: User): void;
 }
--- dist/auth/get-user.decorator.js
+++ dist/auth/get-user.decorator.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.GetUser = void 0;
 const common_1 = require("@nestjs/common");
 exports.GetUser = (0, common_1.createParamDecorator)((data, req) => {
-    return req.user;
+    return req.args[0].user;
 });
 //# sourceMappingURL=get-user.decorator.js.map
